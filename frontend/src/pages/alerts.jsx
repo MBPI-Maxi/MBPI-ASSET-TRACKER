830d43e0ce9a67c95d29928e582914ebf255e846
Maxi <ignaciomaximo11@gmail.com>
2025-05-27 16:51:39 +0800

    msg: 2025-05-27 updated codes

--- frontend/src/pages/alerts.jsx
+++ frontend/src/pages/alerts.jsx
@@ -201,7 +201,7 @@ export function AddAssetInstruction() {
         {/* <li>Use the checkboxes to mark whether the asset is active and physically present.</li> */}
         <li>Use the checkboxes to mark if the asset is active (i.e., currently being used) and if the item is present (i.e., not missing).</li>
         <li>Click <strong>Submit</strong> to save the asset. A success or error message will appear.</li>
-        <li>If the warranty date is not applicable, please enter the same date as the purchase date.</li>
+        <li>If the warranty date is not applicable, please enter the same date as the purchase date. Please don't forget to indicate that in the Remarks section. 😊</li>
         <li>All required fields must be completed before submission.</li>
       </ul>
     </Alert>
